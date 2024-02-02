8f67ce0678b9ea5f99fd7b021ab6d4588d604aad
Josh Justice <me@codingitwrong.com>
2024-02-02 05:49:36 -0500

    Make a copy update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Updated to V2!
         </a>
       </header>
     </div>
